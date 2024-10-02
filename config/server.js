96af05cd3ca3ffc5eb9676aa93074f2d7b330434
Thorbjørn Ellefsen <developer@4reason.no>
2024-10-02 07:09:44 +0200

    enabled debug logging on server

--- config/server.js
+++ config/server.js
@@ -1,8 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  logger: {
+    config: {
+      level: "debug",
+    },
   },
-  
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
